738768a635b7edd6e16f71f6e987fa1c5112246e
nithinsikinam <nithinsikinam@gmail.com>
2021-02-26 01:10:29 +0530

    k

--- google_code.py
+++ google_code.py
@@ -1,7 +1,13 @@
 fyl=open("sample.txt","r")
 k=fyl.readline()
 arr=k.split()
-for i in range(len(arr)):
-    arr.push(int(arr[i]))
 
+for i in range(len(arr)):
+     x= int(arr[i])
+     arr[i]=x
+    
+for x in arr:
+    print(x)
+
+streetno=arr[]
 
