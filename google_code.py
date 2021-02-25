57c390a214136f631482160742e276f606a6a2c5
nithinsikinam <nithinsikinam@gmail.com>
2021-02-26 01:37:33 +0530

    o

--- google_code.py
+++ google_code.py
@@ -13,8 +13,9 @@ arr1=[]
 dict1={}
 for i in range(streetno):
       arr1.append(fyl.readline())
-          
+      x=arr1[i].split()
+      dict1.update({x[2]:[x[0],x[1],x[3]]})
 
-
+print(dict1)
 
 
